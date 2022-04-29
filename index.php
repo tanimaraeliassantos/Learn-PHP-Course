0083cebe344fa4d316cac54bff9d32436962b92f
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-04-29 11:07:58 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,43 @@
-
 <?php
-$last_names = array("Santos", "Silva", "Poppins");
-$first_names = array("Tanimara", "Emma", $last_names);
-echo $first_names[2][1];
+include("variables.php")
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <?php
+        include("navbar.php");
+    ?>    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to Tanimara World!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style
+                component for calling extra attention to featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within
+                the larger container.</p>
+        </div>
+    </div>
+
+  </body>
+  <center>
+    <?php
+        include("footer.php");
+    ?>
+</center>
+<br/>
+</html>
+
 
